c749501d61fc68ef4de7d068193ff1472bbfb6d9
Migarca <migarciacampos@gmail.com>
2023-02-08 19:17:11 +0100

    88 completo

--- src/components/LastEvent/LastEvent.js
+++ src/components/LastEvent/LastEvent.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Icon, Card, Image, Dimmer, Loader, Button, CardContent, CardHeader, CardMeta, CardDescription, ButtonContent } from "semantic-ui-react";
+import './LastEvent.scss';
 
-export default function LastEvent() {
-    return (
-        <div>
+export default function LastEvent({ lastEventsFetch }) {
+    const { loading, result } = lastEventsFetch;
 
-        </div>
-    )
+    //Si esta cargand o y result vacio:
+    if (loading || !result) return <Dimmer active inverted>
+        <Loader inverted>Cargando...</Loader>
+    </Dimmer>
+
+    const { results } = result.data;
+
+    return results.map((event, index) => (
+
+        <Card key={index} className="last-event">
+            <Image src={`${event.thumbnail.path}.${event.thumbnail.extension}`} wrapped ui={false}></Image>
+            <CardContent>
+                <CardHeader>{event.title}</CardHeader>
+                <CardMeta>
+                    <span>
+                        <Icon name="book" />
+                        {event.comics.available} Comics
+                    </span>
+                </CardMeta>
+                <CardDescription>
+                    {event.description}
+                </CardDescription>
+            </CardContent>
+            <CardContent extra>
+                <Button animated fluid as="a" href={event.urls[0].url} target="_blank" color="black">
+                    <ButtonContent visible>Ver Evento</ButtonContent>
+                    <ButtonContent hidden>
+                        <Icon name="arrow right" />
+                    </ButtonContent>
+                </Button>
+            </CardContent>
+        </Card>
+
+    ))
 }
--- src/components/ListLastEvents/ListLastEvents.js
+++ src/components/ListLastEvents/ListLastEvents.js
@@ -2,6 +2,7 @@ import { Header, Card } from "semantic-ui-react";
 import './ListLastEvents.scss'
 import Container from "../Container/Container";
 import useFetch from "../../hooks/useFetch";
+import LastEvent from "../LastEvent/LastEvent";
 
 //Mas abajo en el container le pasa unas props para reutilizar el container con fondo blanco
 
@@ -10,23 +11,13 @@ export default function ListLastEvents() {
     const lastEventsFetch = useFetch(
         `${process.env.REACT_APP_URL_BASE}/events?ts=1&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_ID}&orderBy=startDate&limit=5`
     );
-    console.log(lastEventsFetch);
-
-
     return (
         <div className="container-list-last-events">
             <Header size="large">Últimos eventos</Header>
 
             <Container bg="light">
                 <Card.Group itemsPerRow={5}>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
-                    <p>Los eventos...</p>
+                    <LastEvent lastEventsFetch={lastEventsFetch} />
                 </Card.Group>
             </Container>
 
